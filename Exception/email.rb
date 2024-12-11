adb9d28ae1c30da1e8a66bb7d4366a0e4501f68e
Ayush <ayushnagar633@gmail.com>
2024-12-11 15:15:42 +0530

    [Ayush] ADD: solved gmailcheck using exception handling in ruby

--- Exception/email.rb
+++ Exception/email.rb
@@ -2,7 +2,23 @@
 #  Write a Ruby method validate_email that takes an email address as input and raises an InvalidEmailError 
 #  (custom exception) if the email does not match a standard email format. 
 #  Test the method with various inputs and handle the exception in the caller.
- 
-puts"Enter an Email"
-email = gets.chomp.
+ class InvalidEmailchecker < StandardError; end
+
+def validate_email
+ begin
+ puts"Enter an Email"
+ email = gets.chomp
+ regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+ raise InvalidEmailchecker , "Invalid email format" unless email.match?(regex)
+ puts"Valid email: #{email}"
+
+ rescue InvalidEmailchecker=> e  
+ puts" error occured #{e.message}"
+ puts"pls retry"
+ retry 
+end
+end
+
+validate_email
  
