e50c5af4d78a79fd348db2baff113867f4d066a6
Ayush Nagar <ayushnagar633@gmail.com>
2024-12-20 13:40:05 +0530

    [Ayush]ADD: Searching and sorting in ruby

--- DSA/Searching&sorting/sorting.rb
+++ DSA/Searching&sorting/sorting.rb
@@ -44,6 +44,22 @@ class Sorting
   
     quick_sort(left) + [pivot] + quick_sort(right)
   end
+
+  def merge_sort(arr)
+    return arr if arr.length <= 1
+  
+    mid = arr.length / 2
+    left = merge_sort(arr[0...mid])
+    right = merge_sort(arr[mid..])
+  
+    merge(left, right)
+  end
+  
+  def merge(left, right)
+    sorted = []
+    
+    
+  
 end
 
 \
